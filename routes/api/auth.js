506074ce7cdbfcbc68ebce17cbd6aaf515967d25
Yonatan Snir <yonatan941@gmail.com>
2021-02-01 11:58:14 +0200

    enviroment variable

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middlewere/authMiddlewere");
 const User = require("../../models/User");
-const config = require("config");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
@@ -56,7 +55,7 @@ router.post(
       };
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        process.env.SECRET_TOKEN,
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
